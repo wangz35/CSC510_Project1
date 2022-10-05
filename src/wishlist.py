23a9089288612c7bb068ff402d1026e9db8aa113
priyamshah0709 <53808524+priyamshah0709@users.noreply.github.com>
2022-10-05 15:09:09 -0400

    Update delete_items()

--- src/wishlist.py
+++ src/wishlist.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import sys
 
 class wishlist():
     def __init__(self):
@@ -11,11 +12,12 @@ class wishlist():
         print("============================================================")
         df = pd.read_csv(r'C:\\Users\\piyuj\\OneDrive\\Desktop\\Sem 1\\SE\\PROJ 1\\CSC510_Project1\\src\\item_data.csv')
         print(df['Title'])
+        self.option()
 
     def option(self):
         print("============================================================")
         print ("Enter index number to do follwing function")
-        print ("0 to View all item; 1 to view specific item; 2 to create item; 3 to delete item")
+        print ("0 to View all item; 1 to view specific item; 2 to create item; 3 to delete item; 4 to EXIT CONSOLE")
         input_decision = int(input())
 
         if input_decision == 0:
@@ -29,12 +31,15 @@ class wishlist():
             self.create_item()
             print("ITEM HAS BEEN CREATED")
             self.view_all_items()
-            # self.view_all_items()
 
         elif input_decision == 3:
             print("DELETING ITEM")
             self.delete_item()
-            self.option()
+            print("ITEM HAS BEEN DELETED")
+            self.view_all_items()
+
+        elif input_decision == 4:
+            sys.exit()
 
         else:
             print("INVALID INPUT")
@@ -74,9 +79,14 @@ class wishlist():
         print("======================================================")
         print("INSIDE DELETE ITEM")
         df = pd.read_csv(r'C:\\Users\\piyuj\\OneDrive\\Desktop\\Sem 1\\SE\\PROJ 1\\CSC510_Project1\\src\\item_data.csv')
-        print("ENTER INDEX OF ITEM YOU WANT TO DELETE")
+        print("ENTER INDEX OF ITEM YOU WANT TO DELETE: ")
         input_choice = int(input())
-
-
+        print (len(df.index))
+        if input_choice < len(df.index):
+            df = df.drop(labels=[input_choice], axis=0, inplace=False)
+            df.to_csv(r'C:\\Users\\piyuj\\OneDrive\\Desktop\\Sem 1\\SE\\PROJ 1\\CSC510_Project1\\src\\item_data.csv', index=False)
+        else:
+            print("INVALID INPUT")
+            self.option()
 obj = wishlist()
 obj.option()
