25f8348a224769d52709ce0b98e5cddf14b16f42
yagmurbbayraktar <yagmurbasakbayraktar@gmail.com>
2022-10-04 23:14:32 -0400

    read_item renamed as get_item. delete_item added

--- src/item_manager.py
+++ src/item_manager.py
@@ -6,17 +6,25 @@ class ItemManager:
     def add_item(self, title, desc = '', cost = '', link = ''):
         data = read_csv(self.database)
         lastElement = data['ItemID'].tolist()[-1]+1
-        new_data = {'ItemID': lastElement, 'Title': title, 'Description': desc,'Link': link, 'Cost': cost}
+        new_data = {'ItemID': lastElement, 'Title': title, 'Description': desc,'Link': link, 'Cost': [cost]}
         df = DataFrame(new_data)
         df.to_csv(self.database, mode='a', index=False, header=False)
         print("Data appended successfully.")
 
 
-    def read_item(self, ID: int):
+    def get_item(self, ID: int):
         data = read_csv(self.database)
-        print(data.loc[data['ItemID'] == ID])
+        return (data.loc[data['ItemID'] == ID])
+
+    def delete_item(self, ID: int):
+        data = read_csv(self.database)
+        print(list(data.index[data['ItemID'] == ID]))
+        data.drop(data.index[data['ItemID'] == ID], axis = 0, inplace= True)
+        data.to_csv(self.database, mode='w', index=False, header=False)
+        print("Item deleted.")
 
 
 itemMan = ItemManager()
-#itemMan.add_item('Toaster', '4 slice, with bagel setting', '35', 'amazon')
-itemMan.read_item(2)
+#itemMan.add_item('Toaster', '4 slice, with bagel setting', '35', 'www.amazon.com')
+print(itemMan.get_item(17))
+itemMan.delete_item(17)
