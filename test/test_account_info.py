a3d0183aec1b63615dff22995c5ff9d35cbf8520
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-10-06 15:54:55 -0400

    added some test cases for friend and wishlist

--- test/test_account_info.py
+++ test/test_account_info.py
@@ -1,12 +1,14 @@
 try:
-    from account_info import AccountInfo
+    from account_info import AccountInfo, Friends
 except ImportError as e:
     import sys
     sys.path.append("../CSC510_PROJECT1/src")
-    from account_info import AccountInfo
+    from account_info import AccountInfo, Friends
 
 
 accInfo = AccountInfo()
+friendInfo = Friends()
+
 print('Adding account...')
 created_acc = accInfo.create_account('Ram', 'Bhusal', '12/23/1998', '"Eating pizza, Salsa, Ramen"', '"1, 2, 3, 5"', '"2, 3"')
 _id = created_acc.ID.values[0]
@@ -27,3 +29,13 @@ assert(accInfo.delete_account(_id).all()[0] == 1)
 
 print('Deleting an nonexisting account')
 assert(accInfo.delete_account(300) == -1)
+
+print('Getting friend names....')
+assert(friendInfo.get_friend_names(9)==['David Beckham'])
+assert(friendInfo.get_friend_names(4)==['Jane Doe', 'David Beckham'])
+
+print('Adding some friends......')
+assert(friendInfo.add_friend(9, [2, 3]).FriendList.values[0].replace('"', '').split(', ')[-2:]==['2', '3'])
+
+print('Adding some wishlist items.....')
+assert(accInfo.add_wishlist(9, [4, 9, 10]).WishList.values[0].replace('"', '').split(', ')[-3:]==['4', '9', '10'])
